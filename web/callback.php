fa337b47b067478a718a11aabe2d665a664b3892
hiroponsan <Chuo-univTanaka@outlook.jp>
2016-12-31 16:26:52 +0900

    a

--- web/callback.php
+++ web/callback.php
@@ -17,258 +17,7 @@ if($type != "text"){
 	exit;
 }
 
-else if ($text == '水が欲しい') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "水ですね！　ご予算はいくらでしょうか？ 例. 100円〜500円"
-  ];
-} 
 
-else if ($text == '2人') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "ご予算はいくらですか？ 例. 4000円〜6000円"
-  ];
-} 
-
-else if ($text == '5000円〜6000円') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "メニューはお決まりですか？ 例. お肉"
-  ];
-} 
-
-else if ($text == '100円〜500円') {
-  $response_format_text = [
-  "type" => "template",
-    "altText" => "水になります",
-    "template" => [
-      "type" => "carousel",
-      "columns" => [
-          [
-            "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-1.jpg",
-            "title" => "クリスタルガーザー",
-            "text" => "こちらはいかがでしょう？",
-            "actions" => [
-              [
-                  "type" => "postback",
-                  "label" => "Webを見る",
-                  "data" => "action=rsv&itemid=111"
-              ],
-              [
-                  "type" => "postback",
-                  "label" => "購入する",
-                  "data" => "action=pcall&itemid=111"
-              ],
-              [
-                  "type" => "uri",
-                  "label" => "電話して確認する",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ],
-          [
-            "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-2.jpg",
-            "title" => "いろはす",
-            "text" => "こちらはいかがでしょう？",
-            "actions" => [
-              [
-                  "type" => "postback",
-                  "label" => "Webを見る",
-                  "data" => "action=rsv&itemid=222"
-              ],
-              [
-                  "type" => "postback",
-                  "label" => "購入する",
-                  "data" => "action=pcall&itemid=222"
-              ],
-              [
-                  "type" => "uri",
-                  "label" => "電話して確認する",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ],
-          [
-            "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-3.jpg",
-            "title" => "第3問",
-            "text" => "今ひろぽんはどこにいる？",
-            "actions" => [
-              [
-                  "type" => "postback",
-                  "label" => "大分県",
-                  "data" => "action=rsv&itemid=333"
-              ],
-              [
-                  "type" => "postback",
-                  "label" => "楽天",
-                  "data" => "action=pcall&itemid=333"
-              ],
-              [
-                  "type" => "uri",
-                  "label" => "Amazon",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ]
-      ]
-    ]
-     
-  ];
-} 
-
-else if ($text == '水') {
-  $response_format_text = [
-  "type" => "template",
-    "altText" => "3問あるよ",
-    "template" => [
-      "type" => "carousel",
-      "columns" => [
-          [
-            "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-1.jpg",
-            "title" => "ハンバーグ",
-            "text" => "ハンバーグはいかがでしょう？",
-            "actions" => [
-              [
-                  "type" => "postback",
-                  "label" => "Webを見る",
-                  "data" => "action=rsv&itemid=111"
-              ],
-              [
-                  "type" => "postback",
-                  "label" => "予約する",
-                  "data" => "action=pcall&itemid=111"
-              ],
-              [
-                  "type" => "uri",
-                  "label" => "電話して確認する",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ],
-          [
-            "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-2.jpg",
-            "title" => "ステーキ",
-            "text" => "ステーキはいかがでしょう？",
-            "actions" => [
-              [
-                  "type" => "postback",
-                  "label" => "Webを見る",
-                  "data" => "action=rsv&itemid=222"
-              ],
-              [
-                  "type" => "postback",
-                  "label" => "予約する",
-                  "data" => "action=pcall&itemid=222"
-              ],
-              [
-                  "type" => "uri",
-                  "label" => "電話して確認する",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ],
-          [
-            "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-3.jpg",
-            "title" => "第3問",
-            "text" => "今ひろぽんはどこにいる？",
-            "actions" => [
-              [
-                  "type" => "postback",
-                  "label" => "大分県",
-                  "data" => "action=rsv&itemid=333"
-              ],
-              [
-                  "type" => "postback",
-                  "label" => "楽天",
-                  "data" => "action=pcall&itemid=333"
-              ],
-              [
-                  "type" => "uri",
-                  "label" => "Amazon",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ]
-      ]
-    ]
-     
-  ];
-} 
-
-
-else if ($text == 'はい') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "アカウント名を教えてください"
-  ];
-} 
-
-else if ($text == 'hiropon') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "hiroponさんですね。本人確認のため、電話番号を入力してください。"
-  ];
-} 
-
-
-else if ($text == '000-000-000') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "確認できました。あなたのパスワードは12345です。"
-  ];
-} 
-
-
-//返信データ作成
-else if ($text == 'ひろぽんの体重') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "53キロ"
-  ];
-} 
-
-else if ($text == '明日の天気は？') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "答えはあなたの心の中にある"
-  ];
-} 
-
-else if ($text == 'あなたの運勢') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "信じる者にのみ道は開かれる。 Just do it!"
-  ];
-} 
-
-else if ($text == 'ひろぽんの身長') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "162センチ"
-  ];
-} 
-
-else if ($text == 'ひろぽんの好きな食べ物') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "お寿司"
-  ];
-} 
-
-else if ($text == 'ひろぽんの好きなスポーツ') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "テニス"
-  ];
-} 
-
-else if ($text == 'ひろぽんの趣味') {
-  $response_format_text = [
-     "type" => "text",
-     "text" => "ゲーム"
-  ];
-} 
 
 
 else if ($text == 'いいえ') {
@@ -374,20 +123,25 @@ else if ($text == 'ひろぽんクイズ') {
 } else {
   $response_format_text = [
     "type" => "template",
-    "altText" => "パスワードをお忘れですか？（はい／いいえ）",
+    "altText" => "こんにちわ。今日はどうされましたか？",
     "template" => [
-        "type" => "confirm",
-        "text" => "パスワードをお忘れですか？",
+        "type" => "button",
+        "text" => "こんにちわ。今日はどうされましたか？",
         "actions" => [
             [
               "type" => "message",
-              "label" => "はい",
+              "label" => "面白い話が聞きたい",
               "text" => "はい"
             ],
             [
               "type" => "message",
-              "label" => "いいえ",
-              "text" => "いいえ"
+              "label" => "クイズがしたい",
+              "text" => "クイズがしたい"
+            ],
+            [
+              "type" => "message",
+              "label" => "ご飯が食べたい",
+              "text" => "ご飯が食べたい"
             ]
         ]
     ]
